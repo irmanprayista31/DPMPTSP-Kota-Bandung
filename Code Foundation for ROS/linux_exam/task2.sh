cd921d7e00d382fed552024884542d193b0fd8d1
Irman Prayista <140878493+irmanprayista31@users.noreply.github.com>
2024-10-08 23:18:03 +0700

    Update task2.sh

--- Code Foundation for ROS/linux_exam/task2.sh
+++ Code Foundation for ROS/linux_exam/task2.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+#read -p "Enter Option: " USER_OPTION
 
+if [[ "$1" == "small_square" ]]; then
+rosrun linux_exam small_square.py
+elif [[ "$1" == "medium_square" ]]; then
+rosrun linux_exam medium_square.py
+elif [[ "$1" == "big_square" ]]; then
+rosrun linux_exam big_square.py
+else
+	echo "Please Provide Valid Parameter"
+fi
