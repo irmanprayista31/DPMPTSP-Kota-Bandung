e1642c85306cbbbc14fe5a26cc776786f16aee83
Irman Prayista <140878493+irmanprayista31@users.noreply.github.com>
2025-01-24 08:36:57 +0700

    Delete Code Foundation for ROS directory

--- Code Foundation for ROS/linux_exam/task1.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd /home/user/catkin_ws/src/linux_exam
-mkdir -p this/is/my/linux/exam
-cd /home/user/catkin_ws/src/linux_exam/this/is/my/linux/exam
-touch my_file.py
-echo "This bash script has finished!"
